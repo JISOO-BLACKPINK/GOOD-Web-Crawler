67b3e16d12cd845cae26315b4bc4a8f12fa59b3a
JISOO <64460924+JISOO-BLACKPINK@users.noreply.github.com>
2021-07-23 09:45:47 +0800

    Update pc.py
    原来的网站被ban了，而且下载效率太低

--- pc.py
+++ pc.py
@@ -1,36 +1,46 @@
-import re
+import os
 import urllib.error
 import urllib.request
 import urllib.response
+import sys
 from bs4 import BeautifulSoup
 def main():
     dataknow()
 #数据解析
 def dataknow():
-    jak=0
-    a=0
-    while True:
-        lax=[]
-        a+=1
-        num=14000-a
-        strn=str(num)
-        turl="http://20a5.com/html/article/index"+strn+".html"
-        print(turl)
-        au=gethtml(turl)
-        bs=BeautifulSoup(au,"html.parser")
-        lists=bs.find_all("img")
-        for k in lists:
-            lax.append(k.get("src"))
-        for dw in lax:
-            jak+=1
-            with open("D:\\oprea\\"+str(jak)+".jpg","wb") as imgs:
+    if  not os.path.exists("F:\\pic\\"):
+        os.makedirs("F:\\pic\\") 
+    else:
+        print("文件夹已经存在了，请把原来的文件夹删了，路径：F:\\pic\\，不然用不了")
+        sys.exit()
+
+    basicnum=10883
+    lobol=0
+    while(True):
+     url="http://www.gan1994.com/html/article/index"+str(basicnum)+".html"
+     sorce=gethtml(url)
+     bs=BeautifulSoup(sorce,"html")
+     imglist=bs.find_all('img')
+     weblist=[]
+     for a in imglist:
+         web=a.get("src")
+         weblist.append(web)
+     for b in weblist:
+        lobol+=1
+        with open("F:\\pic\\"+str(lobol)+".jpg","wb") as imgs:
+                print("正在下载第"+str(lobol)+"张图")
                 try:
-                    lka=urllib.request.urlopen(dw)
+                    lka=urllib.request.urlopen(b)
+                    imgs.write(lka.read())
                 except:
-                    print("ERROR")
-                print("正在下载第"+str(jak)+"张图")
-                imgs.write(lka.read())
+                    print("ERROR IN"+str(lobol)+"1")
+                    imgs.close()
+                    os.remove("F:\\pic\\"+str(lobol)+".jpg")
+               
                 print("下载成功")
+     basicnum-=1
+     
+               
 #获取源代码        
 def gethtml(url):
     head={"users":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
@@ -40,7 +50,7 @@ def gethtml(url):
         rep=urllib.request.urlopen(req)
         p=rep.read().decode("GBK")
     except:
-        print("ERROR!")
+        print("Error in 2")
     return p
 if __name__=="__main__":
     main()
